2b3f733deaf8af0896e7a593066127771e1f1cd4
Ciprian Dorin Craciun <ciprian@volution.ro>
2022-08-06 11:37:26 +0300

    [libs/errors]  Add a `anyhow::bail!` benchmarks.

--- sources/errors/benches_public.rs
+++ sources/errors/benches_public.rs
@@ -16,6 +16,8 @@ mod api {
 	use ::vrl_errors::*;
 	
 	use ::anyhow::Error as AnyhowError;
+	use ::anyhow::Result as AnyhowResult;
+	use ::anyhow::bail as anyhow_bail;
 	
 	use ::test::Bencher;
 	use ::test::black_box;
@@ -26,54 +28,75 @@ mod api {
 	
 	
 	#[ bench ]
-	fn create_error_with_code (_bencher : &mut Bencher) -> () {
+	fn create_error_new_with_code (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || TestError::new_with_code (0xdbe85382));
 	}
 	
 	#[ bench ]
-	fn create_error_with_static_message (_bencher : &mut Bencher) -> () {
+	fn create_error_new_with_static_message (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || TestError::new_with_message (0x8aeed0a9, "with static message"));
 	}
 	
 	#[ bench ]
-	fn create_error_with_boxed_message (_bencher : &mut Bencher) -> () {
+	fn create_error_new_with_boxed_message (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || TestError::new_with_message (0x8aeed0a9, "with boxed message".to_string ()));
 	}
 	
 	#[ bench ]
-	fn create_error_with_static_format (_bencher : &mut Bencher) -> () {
+	fn create_error_new_with_static_format (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || TestError::new_with_format (0x3203195a, format_args! ("with static format")));
 	}
 	
 	#[ bench ]
-	fn create_error_with_boxed_format (_bencher : &mut Bencher) -> () {
-		run_bench (_bencher, || TestError::new_with_format (0xce752d4f, format_args! ("with boxed format {}", 0)));
+	fn create_error_new_with_boxed_format (_bencher : &mut Bencher) -> () {
+		run_bench (_bencher, || TestError::new_with_format (0xce752d4f, format_args! ("with boxed format {}", 42)));
 	}
 	
 	#[ bench ]
-	fn create_error_with_details (_bencher : &mut Bencher) -> () {
+	fn create_error_new_with_details (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || TestErrorWithDetails::new_with_code (0xa7112069) .with_details (42));
 	}
 	
 	
 	#[ bench ]
-	fn create_anyhow_with_void (_bencher : &mut Bencher) -> () {
+	fn create_anyhow_new_with_cause (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || AnyhowError::new (::std::fmt::Error));
 	}
 	
 	#[ bench ]
-	fn create_anyhow_with_static_message (_bencher : &mut Bencher) -> () {
+	fn create_anyhow_new_with_static_message (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || AnyhowError::msg ("with static message"));
 	}
 	
 	#[ bench ]
-	fn create_anyhow_with_boxed_message (_bencher : &mut Bencher) -> () {
+	fn create_anyhow_new_with_boxed_message (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || AnyhowError::msg ("with boxed message".to_string ()));
 	}
 	
 	#[ bench ]
-	fn create_anyhow_with_context (_bencher : &mut Bencher) -> () {
+	fn create_anyhow_new_with_context (_bencher : &mut Bencher) -> () {
 		run_bench (_bencher, || AnyhowError::new (::std::fmt::Error) .context (42));
+	}
+	
+	
+	#[ bench ]
+	fn create_anyhow_bail_with_cause (_bencher : &mut Bencher) -> () {
+		run_bench (_bencher, || -> AnyhowResult<()> { anyhow_bail! (::std::fmt::Error); });
+	}
+	
+	#[ bench ]
+	fn create_anyhow_bail_with_static_message (_bencher : &mut Bencher) -> () {
+		run_bench (_bencher, || -> AnyhowResult<()> { anyhow_bail! ("with static message"); });
+	}
+	
+	#[ bench ]
+	fn create_anyhow_bail_with_boxed_message (_bencher : &mut Bencher) -> () {
+		run_bench (_bencher, || -> AnyhowResult<()> { anyhow_bail! ("with boxed message".to_string ()); });
+	}
+	
+	#[ bench ]
+	fn create_anyhow_bail_with_boxed_format (_bencher : &mut Bencher) -> () {
+		run_bench (_bencher, || -> AnyhowResult<()> { anyhow_bail! ("with boxed format {}", 42); });
 	}
 	
 	
