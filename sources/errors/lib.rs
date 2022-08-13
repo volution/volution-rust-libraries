1eaa6990015fe6320df0a2f1b9a7d90a30e45bcf
Ciprian Dorin Craciun <ciprian@volution.ro>
2022-08-13 17:48:47 +0300

    [lib/errors]  Refactor error definition macros into simpler ones.

--- sources/errors/lib.rs
+++ sources/errors/lib.rs
@@ -16,7 +16,6 @@ pub(crate) mod tests_internal;
 
 
 define_error! (pub UnexpectedError, application : 0x_110a195d8bca29ca8f300ae605633681, module : 0x_577ad068, type : 0xdd902b5d);
-
 define_error! (pub PanicError, application : 0x_110a195d8bca29ca8f300ae605633681, module : 0x_577ad068, type : 0x762a6a31);
 
 
--- sources/errors/macros.rs
+++ sources/errors/macros.rs
@@ -5,6 +5,10 @@
 
 
 
+
+
+
+
 #[ macro_export ]
 macro_rules! define_error {
 	
@@ -25,26 +29,52 @@ macro_rules! define_error {
 	};
 	
 	
+	( $_visibility : vis $_error_type : ident $( , application : $_application_code : literal )? $( , module : $_module_code : literal )? $( , type : $_type_code : literal )? ) => {
+		
+		$crate::define_error_type! ($_visibility $_error_type $( , application : $_application_code )? $( , module : $_module_code )? $( , type : $_type_code )? );
+		$crate::define_error_extensions! ($_error_type);
+		$crate::define_error_conversions! ($_error_type);
+	};
+	
+	
 	( $_visibility : vis $_error_type : ident < $_details_type : ty > $( , application : $_application_code : literal )? $( , module : $_module_code : literal )? $( , type : $_type_code : literal )? ) => {
 		
-		$crate::define_error! ($_visibility $_error_type $( , application : $_application_code )? $( , module : $_module_code )? $( , type : $_type_code )? );
+		$crate::define_error_type! ($_visibility $_error_type < $_details_type > $( , application : $_application_code )? $( , module : $_module_code )? $( , type : $_type_code )? );
+		$crate::define_error_extensions! ($_error_type < $_details_type >);
+		$crate::define_error_conversions! ($_error_type);
+	};
+}
+
+
+
+
+#[ macro_export ]
+macro_rules! define_error_type {
+	
+	
+	( $_visibility : vis $_error_type : ident < $_details_type : ty > $( , application : $_application_code : literal )? $( , module : $_module_code : literal )? $( , type : $_type_code : literal )? ) => {
+		
+		
+		$crate::define_error_type! ($_visibility $_error_type $( , application : $_application_code )? $( , module : $_module_code )? $( , type : $_type_code )? );
+		
 		
 		impl $crate::ErrorWithDetails for $_error_type {
 			type Details = $_details_type;
 		}
 		
+		
 		impl $crate::ErrorNewWithDetails for $_error_type {}
 	};
 	
 	
-	( $_visibility : vis $_type : ident $( , application : $_application_code : literal )? $( , module : $_module_code : literal )? $( , type : $_type_code : literal )? ) => {
+	( $_visibility : vis $_error_type : ident $( , application : $_application_code : literal )? $( , module : $_module_code : literal )? $( , type : $_type_code : literal )? ) => {
 		
 		
 		#[ must_use ]
-		$_visibility struct $_type ($crate::ErrorInternals<$_type>);
+		$_visibility struct $_error_type ($crate::ErrorInternals<$_error_type>);
 		
 		
-		impl $_type {
+		impl $_error_type {
 			
 			#![ allow (unused_must_use) ]
 			#![ allow (path_statements) ]
@@ -55,7 +85,178 @@ macro_rules! define_error {
 		}
 		
 		
-		impl $_type {
+		impl $crate::Error for $_error_type {
+			
+			fn from_internals (_internals : $crate::ErrorInternals<Self>) -> Self {
+				Self (_internals)
+			}
+			
+			fn into_internals (self) -> $crate::ErrorInternals<Self> {
+				self.0
+			}
+			
+			fn internals_ref (&self) -> &$crate::ErrorInternals<Self> {
+				&self.0
+			}
+			
+			fn internals_mut (&mut self) -> &mut $crate::ErrorInternals<Self> {
+				&mut self.0
+			}
+		}
+		
+		
+		impl $crate::ErrorNew for $_error_type {
+			
+			fn new_with_code (_error_code : impl ::std::convert::Into<$crate::ErrorCode>) -> Self {
+				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_code (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into ()))
+			}
+			
+			fn new_with_message (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _message : impl ::std::convert::Into<::std::borrow::Cow<'static, str>>) -> Self {
+				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_message (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _message.into ()))
+			}
+			
+			fn new_with_format (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _format : ::std::fmt::Arguments) -> Self {
+				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_format (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _format))
+			}
+			
+			fn new_with_cause <E> (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _cause : E) -> Self
+					where E : ::std::error::Error + ::std::marker::Sync + ::std::marker::Send + 'static
+			{
+				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_cause (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _cause))
+			}
+			
+			fn new_with_message_and_cause <E> (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _message : impl ::std::convert::Into<::std::borrow::Cow<'static, str>>, _cause : E) -> Self
+					where E : ::std::error::Error + ::std::marker::Sync + ::std::marker::Send + 'static
+			{
+				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_message_and_cause (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _message.into (), _cause))
+			}
+			
+			fn new_with_format_and_cause <E> (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _format : ::std::fmt::Arguments, _cause : E) -> Self
+					where E : ::std::error::Error + ::std::marker::Sync + ::std::marker::Send + 'static
+			{
+				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_format_and_cause (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _format, _cause))
+			}
+		}
+		
+		
+		impl ::std::error::Error for $_error_type {}
+		
+		
+		impl ::std::fmt::Display for $_error_type {
+			
+			fn fmt (&self, _formatter : &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+				<Self as $crate::Error>::display_fmt (self, _formatter)
+			}
+		}
+		
+		
+		impl ::std::fmt::Debug for $_error_type {
+			
+			fn fmt (&self, _formatter : &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+				<Self as $crate::Error>::debug_fmt (self, _formatter)
+			}
+		}
+	};
+}
+
+
+
+
+#[ macro_export ]
+macro_rules! define_error_extensions {
+	
+	
+	( $_error_type : ident < $_details_type : ty > ) => {
+		
+		
+		$crate::define_error_extensions! ($_error_type);
+		
+		
+		impl $_error_type {
+			
+			#[ must_use ]
+			pub fn details (&self) -> ::std::option::Option<<Self as $crate::ErrorWithDetails>::Details> where <Self as $crate::ErrorWithDetails>::Details : ::std::marker::Copy {
+				<Self as $crate::ErrorWithDetails>::details (self)
+			}
+			
+			#[ must_use ]
+			pub fn details_cloned (&self) -> ::std::option::Option<<Self as $crate::ErrorWithDetails>::Details> where <Self as $crate::ErrorWithDetails>::Details : ::std::clone::Clone {
+				<Self as $crate::ErrorWithDetails>::details_cloned (self)
+			}
+			
+			#[ must_use ]
+			pub fn details_ref (&self) -> ::std::option::Option<&<Self as $crate::ErrorWithDetails>::Details> {
+				<Self as $crate::ErrorWithDetails>::details_ref (self)
+			}
+		}
+		
+		
+		impl $_error_type {
+			
+			#[ must_use ]
+			pub fn with_details (self, _details : <Self as $crate::ErrorWithDetails>::Details) -> Self {
+				<Self as $crate::ErrorNewWithDetails>::with_details (self, _details)
+			}
+		}
+	};
+	
+	
+	( $_error_type : ident ) => {
+		
+		
+		impl $_error_type {
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn application_code (&self) -> $crate::ErrorApplicationCode {
+				<Self as $crate::Error>::application_code (self)
+			}
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn module_code (&self) -> $crate::ErrorModuleCode {
+				<Self as $crate::Error>::module_code (self)
+			}
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn type_code (&self) -> $crate::ErrorTypeCode {
+				<Self as $crate::Error>::type_code (self)
+			}
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn error_code (&self) -> $crate::ErrorCode {
+				<Self as $crate::Error>::error_code (self)
+			}
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn message_string (&self) -> ::std::option::Option<::std::borrow::Cow<str>> {
+				<Self as $crate::Error>::message_string (self)
+			}
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn cause_ref (&self) -> ::std::option::Option<&(dyn $crate::StdError + ::std::marker::Send + ::std::marker::Sync + 'static)> {
+				<Self as $crate::Error>::cause_ref (self)
+			}
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn into_result <V> (self) -> ::std::result::Result<V, Self> {
+				<Self as $crate::Error>::into_result (self)
+			}
+			
+			#[ allow (dead_code) ]
+			#[ must_use ]
+			pub fn into_std_io_error (self) -> $crate::StdIoError {
+				<Self as $crate::Error>::into_std_io_error (self)
+			}
+		}
+		
+		
+		impl $_error_type {
 			
 			#[ allow (dead_code) ]
 			#[ must_use ]
@@ -101,94 +302,82 @@ macro_rules! define_error {
 		}
 		
 		
-		impl $crate::ErrorNew for $_type {
+		impl $_error_type {
 			
-			#[ must_use ]
-			fn new_with_code (_error_code : impl ::std::convert::Into<$crate::ErrorCode>) -> Self {
-				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_code (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into ()))
+			#[ allow (dead_code) ]
+			pub fn panic (self, _code : impl ::std::convert::Into<$crate::ErrorCode>) -> ! {
+				<Self as $crate::ErrorExtPanic>::panic (self, _code)
 			}
 			
-			#[ must_use ]
-			fn new_with_message (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _message : impl ::std::convert::Into<::std::borrow::Cow<'static, str>>) -> Self {
-				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_message (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _message.into ()))
+			#[ allow (dead_code) ]
+			pub fn panic_with_message (self, _code : impl ::std::convert::Into<$crate::ErrorCode>, _message : impl ::std::convert::Into<::std::borrow::Cow<'static, str>>) -> ! {
+				<Self as $crate::ErrorExtPanic>::panic_with_message (self, _code, _message)
 			}
 			
-			#[ must_use ]
-			fn new_with_format (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _format : ::std::fmt::Arguments) -> Self {
-				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_format (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _format))
+			#[ allow (dead_code) ]
+			pub fn panic_with_format (self, _code : impl ::std::convert::Into<$crate::ErrorCode>, _format : ::std::fmt::Arguments) -> ! {
+				<Self as $crate::ErrorExtPanic>::panic_with_format (self, _code, _format)
 			}
 			
-			#[ must_use ]
-			fn new_with_cause <E> (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _cause : E) -> Self
-					where E : ::std::error::Error + ::std::marker::Sync + ::std::marker::Send + 'static
-			{
-				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_cause (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _cause))
+			#[ allow (dead_code) ]
+			pub fn infallible (self, _code : impl ::std::convert::Into<$crate::ErrorCode>) -> ! {
+				<Self as $crate::ErrorExtPanic>::infallible (self, _code)
 			}
 			
-			#[ must_use ]
-			fn new_with_message_and_cause <E> (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _message : impl ::std::convert::Into<::std::borrow::Cow<'static, str>>, _cause : E) -> Self
-					where E : ::std::error::Error + ::std::marker::Sync + ::std::marker::Send + 'static
-			{
-				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_message_and_cause (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _message.into (), _cause))
+			#[ allow (dead_code) ]
+			pub fn panic_0 (self) -> ! {
+				<Self as $crate::ErrorExtPanic>::panic_0 (self)
 			}
 			
-			#[ must_use ]
-			fn new_with_format_and_cause <E> (_error_code : impl ::std::convert::Into<$crate::ErrorCode>, _format : ::std::fmt::Arguments, _cause : E) -> Self
-					where E : ::std::error::Error + ::std::marker::Sync + ::std::marker::Send + 'static
-			{
-				<Self as $crate::Error>::from_internals ($crate::ErrorInternals::new_with_format_and_cause (Self::APPLICATION_CODE, Self::MODULE_CODE, Self::TYPE_CODE, _error_code.into (), _format, _cause))
+			#[ allow (dead_code) ]
+			pub fn infallible_0 (self) -> ! {
+				<Self as $crate::ErrorExtPanic>::infallible_0 (self)
 			}
 		}
 		
 		
-		impl $crate::Error for $_type {
+		impl $_error_type {
 			
-			#[ doc (hidden) ]
+			#[ allow (dead_code) ]
 			#[ must_use ]
-			fn from_internals (_internals : $crate::ErrorInternals<Self>) -> Self {
-				Self (_internals)
+			pub fn else_wrap <E : $crate::ErrorNew> (self, _code : impl ::std::convert::Into<$crate::ErrorCode>) -> E {
+				<Self as $crate::ErrorExtWrap<E>>::else_wrap (self, _code)
 			}
 			
-			#[ doc (hidden) ]
+			#[ allow (dead_code) ]
 			#[ must_use ]
-			fn into_internals (self) -> $crate::ErrorInternals<Self> {
-				self.0
+			pub fn else_wrap_with_message <E : $crate::ErrorNew> (self, _code : impl ::std::convert::Into<$crate::ErrorCode>, _message : impl ::std::convert::Into<::std::borrow::Cow<'static, str>>) -> E {
+				<Self as $crate::ErrorExtWrap<E>>::else_wrap_with_message (self, _code, _message)
 			}
 			
-			#[ doc (hidden) ]
+			#[ allow (dead_code) ]
 			#[ must_use ]
-			fn internals_ref (&self) -> &$crate::ErrorInternals<Self> {
-				&self.0
+			pub fn else_wrap_with_format <E : $crate::ErrorNew> (self, _code : impl ::std::convert::Into<$crate::ErrorCode>, _format : ::std::fmt::Arguments) -> E {
+				<Self as $crate::ErrorExtWrap<E>>::else_wrap_with_format (self, _code, _format)
 			}
+		}
+	};
+}
+
+
+
+
+#[ macro_export ]
+macro_rules! define_error_conversions {
+	
+	
+	( $_error_type : ident ) => {
+		
+		
+		impl <V> ::std::convert::Into<::std::result::Result<V, Self>> for $_error_type {
 			
-			#[ doc (hidden) ]
-			#[ must_use ]
-			fn internals_mut (&mut self) -> &mut $crate::ErrorInternals<Self> {
-				&mut self.0
+			fn into (self) -> ::std::result::Result<V, Self> {
+				<Self as $crate::Error>::into_result (self)
 			}
 		}
 		
 		
-		impl ::std::error::Error for $_type {}
-		
-		
-		impl ::std::fmt::Display for $_type {
-			
-			fn fmt (&self, _formatter : &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-				<Self as $crate::Error>::display_fmt (self, _formatter)
-			}
-		}
-		
-		
-		impl ::std::fmt::Debug for $_type {
-			
-			fn fmt (&self, _formatter : &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-				<Self as $crate::Error>::debug_fmt (self, _formatter)
-			}
-		}
-		
-		
-		impl ::std::convert::Into<::std::io::Error> for $_type {
+		impl ::std::convert::Into<::std::io::Error> for $_error_type {
 			
 			fn into (self) -> ::std::io::Error {
 				<Self as $crate::Error>::into_std_io_error (self)
@@ -196,7 +385,7 @@ macro_rules! define_error {
 		}
 		
 		
-		impl ::std::convert::Into<::std::process::ExitCode> for $_type {
+		impl ::std::convert::Into<::std::process::ExitCode> for $_error_type {
 			
 			fn into (self) -> ::std::process::ExitCode {
 				<Self as $crate::Error>::into_std_process_exit_code (self)
@@ -204,7 +393,7 @@ macro_rules! define_error {
 		}
 		
 		
-		impl ::std::process::Termination for $_type {
+		impl ::std::process::Termination for $_error_type {
 			
 			fn report (self) -> ::std::process::ExitCode {
 				<Self as $crate::Error>::into_std_process_exit_code (self)
@@ -212,6 +401,10 @@ macro_rules! define_error {
 		}
 	};
 }
+
+
+
+
 
 
 
