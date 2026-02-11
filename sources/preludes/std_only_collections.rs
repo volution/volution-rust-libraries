6cca802b37eef89f5db21294fc44fa964df1d10c
Ciprian Dorin Craciun <ciprian@volution.ro>
2026-02-11 14:50:04 +0200

    [lib/preludes][feature]  Use some collection related modules.

--- sources/preludes/std_only_collections.rs
+++ sources/preludes/std_only_collections.rs
@@ -4,7 +4,23 @@ pub use ::std::vec;
 
 pub use ::std::vec::Vec;
 
+pub use ::std::slice;
+
 pub use ::std::collections;
+
+pub use ::std::collections::{
+		
+		hash_map,
+		hash_set,
+		
+		btree_map,
+		btree_set,
+		
+		binary_heap,
+		linked_list,
+		
+		vec_deque,
+	};
 
 pub use ::std::collections::{
 		
