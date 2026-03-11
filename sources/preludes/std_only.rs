c00d5d2df47472a2835c53c1f76218e3b53aaea7
Ciprian Dorin Craciun <ciprian@volution.ro>
2026-03-11 16:11:01 +0200

    [lib/preludes][refactor]  Instead of re-exporting `::std` preludes, explicitly export what is present in them (in a try not to re-export the `panic` macro).

--- sources/preludes/std_only.rs
+++ sources/preludes/std_only.rs
@@ -1,6 +1,49 @@
 
 
 pub use ::std;
-pub use ::std::prelude::rust_2024::*;
+
+
+
+
+// pub use ::std::prelude::v1::*;
+
+pub use ::std::marker::{ Copy, Send, Sized, Sync, Unpin, };
+
+pub use ::std::ops::{ Drop, Fn, FnMut, FnOnce, };
+
+pub use ::std::ops::{ AsyncFn, AsyncFnMut, AsyncFnOnce, };
+
+pub use ::std::clone::{ Clone, };
+
+pub use ::std::cmp::{ Eq, Ord, PartialEq, PartialOrd, };
+
+pub use ::std::convert::{ AsMut, AsRef, From, Into, };
+
+pub use ::std::default::{ Default, };
+
+pub use ::std::iter::{ DoubleEndedIterator, ExactSizeIterator, };
+
+pub use ::std::iter::{ Extend, IntoIterator, Iterator, };
+
+pub use ::std::option::Option::{ self, None, Some, };
+
+pub use ::std::result::Result::{ self, Err, Ok, };
+
+pub use ::std::boxed::{ Box, };
+
+pub use ::std::string::{ String, ToString, };
+
+pub use ::std::vec::{ Vec, };
+
+
+
+
+// pub use ::std::prelude::rust_2024::*;
+
+pub use ::std::iter::{ FromIterator, };
+
+pub use ::std::convert::{ TryFrom, TryInto, };
+
+pub use ::std::future::{ Future, IntoFuture, };
 
 
